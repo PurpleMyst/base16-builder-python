32e9ea9a28cd9de23f6ebe1434383476f149e9b2
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-04-05 15:59:24 +0200

    refactor: Extract a lot of code into functions

--- educe/__main__.py
+++ educe/__main__.py
@@ -9,6 +9,8 @@ def main():
     elif sys.argv[1] == "build":
         scheme = colorparser.generate_colorscheme(sys.argv[2])
         builder.build(scheme)
+    else:
+        raise RuntimeError("no command given")
 
 
 if __name__ == "__main__":
--- educe/colorparser.py
+++ educe/colorparser.py
@@ -1,15 +1,11 @@
 import colorsys
 import math
 import random
-from collections import namedtuple
 
 import sklearn.cluster
 import numpy
 
 from PIL import Image
-
-Point = namedtuple("Point", ("coords", "ct"))
-Cluster = namedtuple("Cluster", ("points", "center"))
 
 IDEAL_COLORS = {
     # black
@@ -35,75 +31,88 @@ def rgb_to_hex(rgb):
     return "".join(format(p, "02x") for p in rgb)
 
 
+def rgb_to_hsv(rgb):
+    return colorsys.rgb_to_hsv(*map(lambda x: x / 255, rgb))
+
+
+def hsv_to_rgb(hsv):
+    return tuple(map(lambda x: int(x * 255), colorsys.hsv_to_rgb(*hsv)))
+
+
 def lerp(x1, x2, step):
     return x1 + step * (x2 - x1)
 
 
 def gradient(start, end, steps):
-    h1, s1, v1 = colorsys.rgb_to_hsv(*map(lambda x: x / 256, start))
-    h2, s2, v2 = colorsys.rgb_to_hsv(*map(lambda x: x / 256, end))
+    h1, s1, v1 = rgb_to_hsv(start)
+    h2, s2, v2 = rgb_to_hsv(end)
 
     for i in range(steps):
-        h = lerp(h1, h2, (1 / steps) * i)
-        s = lerp(s1, s2, (1 / steps) * i)
-        v = lerp(v1, v2, (1 / steps) * i)
+        step = (1 / steps) * i
+        h = lerp(h1, h2, step)
+        s = lerp(s1, s2, step)
+        v = lerp(v1, v2, step)
 
-        yield tuple(map(lambda x: int(x * 256), colorsys.hsv_to_rgb(h, s, v)))
-
-
-def euclidean_dist(p1, p2):
-    return math.sqrt(sum((c1 - c2) ** 2 for c1, c2 in zip(p1.coords, p2.coords)))
+        yield hsv_to_rgb((h, s, v))
 
 
-def color_dist(c1, c2):
+def dist(c1, c2):
     return math.sqrt(sum((c1 - c2) ** 2 for c1, c2 in zip(c1, c2)))
 
 
-def normalize(rgb, minv=128, maxv=256):
-    h, s, v = colorsys.rgb_to_hsv(*map(lambda x: x / 256, rgb))
-
-    v = min(max(v, minv / 256), maxv / 256)
-
-    rgb = map(lambda x: int(x * 256), colorsys.hsv_to_rgb(h, s, v))
-
-    return tuple(rgb)
+def clamp_value(rgb, minv, maxv):
+    h, s, v = rgb_to_hsv(rgb)
+    v = min(max(v, minv / 255), maxv / 255)
+    return hsv_to_rgb((h, s, v))
 
 
-def generate_colorscheme(wallpaper_file):
-    img = Image.open(wallpaper_file)
+def generate_palette(img_path, colors):
+    img = Image.open(img_path)
     img.thumbnail((256, 256))
     w, h = img.size
 
     points = numpy.array(
         [[*color, count] for count, color in img.getcolors(maxcolors=w * h)]
     )
-    kmeans = sklearn.cluster.KMeans(n_clusters=16, n_jobs=-1).fit(points)
-    colors = [tuple(map(int, color[:3])) for color in kmeans.cluster_centers_]
+    kmeans = sklearn.cluster.KMeans(n_clusters=colors, n_jobs=-1).fit(points)
 
+    colors = [tuple(map(int, color[:3])) for color in kmeans.cluster_centers_]
+    return colors
+
+
+def normalize_colors(colors):
     normalized_colors = []
+
     for i, color in enumerate(colors):
         if i == 0:
-            color = normalize(color, minv=0, maxv=32)
+            color = clamp_value(color, minv=0, maxv=32)
         elif i < 8:
-            color = normalize(color, minv=160, maxv=224)
+            color = clamp_value(color, minv=160, maxv=224)
         elif i == 8:
-            color = normalize(color, minv=128, maxv=192)
+            color = clamp_value(color, minv=128, maxv=192)
         elif i < 16:
-            color = normalize(color, minv=200, maxv=256)
+            color = clamp_value(color, minv=200, maxv=255)
+
         normalized_colors.append(color)
+
+    return normalized_colors
+
+
+def min_with_idx(l, *, key=lambda x: x):
+    return min(enumerate(l), key=lambda item: key(item[1]))
+
+
+def generate_colorscheme(img_path):
+    colors = generate_palette(img_path, 16)
+    colors = normalize_colors(colors)
 
     bases = {f"base{i:02X}": None for i in range(16)}
 
     # Find the colors which are the closest to the colors we want.
     # For example: base08 is meant to be red, so we find the "reddest" color.
     for base, ideal_color in IDEAL_COLORS.items():
-        idx, color = min(
-            enumerate(normalized_colors),
-            key=lambda item: color_dist(ideal_color, item[1]),
-        )
-
-        del normalized_colors[idx]
-
+        idx, color = min_with_idx(colors, key=lambda color: dist(ideal_color, color))
+        del colors[idx]
         bases[base] = color
 
     # Create a gradient from blackest to whitest for bases 00 to 07, to make
@@ -113,10 +122,10 @@ def generate_colorscheme(wallpaper_file):
         bases[f"base{i:02X}"] = base
 
     # Other colors have no special meaning, so we just take random ones.
-    random.shuffle(normalized_colors)
+    random.shuffle(colors)
     for base in bases:
         if bases[base] is None:
-            bases[base] = normalized_colors.pop()
+            bases[base] = colors.pop()
 
     colorscheme = {base: rgb_to_hex(color) for base, color in bases.items()}
     colorscheme.update({"scheme": "wallpaper", "author": "PurpleMyst"})
